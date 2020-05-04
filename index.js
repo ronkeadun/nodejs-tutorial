176eca4a735fc8699f1edb2d4aa0c22333a845aa
ronkeadun <ronkeadun1@yahoo.com>
2020-05-04 12:56:35 +0100

    feature(server): added view engine and homepage endpoint for todo app

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("this is my index file");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "My Todo Application",
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.send("This is our login page");
+});
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
