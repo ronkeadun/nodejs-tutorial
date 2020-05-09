6bb1f7ba7c320954e7a5985c2b492f1d7d6a6220
ronkeadun <ronkeadun1@yahoo.com>
2020-05-09 14:55:32 +0100

    feature(server):added structure for the database

---
+++ models/tasks.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+require("./../config");
+
+const TaskSchema = new mongoose.Schema(
+  {
+    name: mongoose.SchemaTypes.String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", TaskSchema, "tasks");
+
+/*const task = new Task({
+  name: "Friday Clubbing",
+});
+
+task
+  .save()
+  .then((doc) => {
+    console.log("Document saved", doc);
+  })
+  .catch((error) => {
+    console.error(error);
+  });*/
+
+/*task.save((error) => {
+  if (error) {
+    console.log("something went wrong");
+  } else {
+    console.log("data has been saved");
+  }
+});*/
+
+module.exports = {
+  TaskSchema,
+  Task,
+};
